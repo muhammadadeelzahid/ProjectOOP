d7c09b41eb8d79d6b97f8a4289e94afba610acf5
Adeel Zahid <47818499+muhammadadeelzahid@users.noreply.github.com>
2020-04-26 21:32:06 +0500

    Initial commit

---
+++ someProject.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : someProject.cpp
+// Author      : Adeel
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Wilkomen! Gutten Morgen" << endl; // prints Wilkomen! Gutten Morgen
+	cout<<"Whatsupp bro"<<endl ;
+	return 0;
+}
